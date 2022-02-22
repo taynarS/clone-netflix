3863a433663a1d74aec2d93f3a9c4a855f5d4493
Taynar <taynarsantiago@gmail.com>
2022-02-22 10:11:28 -0300

    primeiro commit

---
+++ js/owl/setup.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            item:1
+        },
+        600:{
+            item:3
+        },
+        1000:{
+            item:5
+        }
+    }
+})
